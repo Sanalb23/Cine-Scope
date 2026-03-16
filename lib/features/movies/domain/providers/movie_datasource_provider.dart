a538353b3a5f4cf54a7ba9182f78bb8ef80d1257
Santino Albornoz <sanalbor32@gmail.com>
2026-03-16 14:40:02 -0300

    inject httpClientProvider into movieDatasourceProvider

--- lib/features/movies/domain/providers/movie_datasource_provider.dart
+++ lib/features/movies/domain/providers/movie_datasource_provider.dart
@@ -1,7 +1,9 @@
+import 'package:cine_scope/core/providers/http_client_provider.dart';
 import 'package:cine_scope/features/movies/data/datasource/movie_datasource.dart';
 import 'package:cine_scope/features/movies/data/datasource/tmdb_datasource.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final movieDatasourceProvider = Provider.autoDispose<MovieDatasource>(
-  (ref) => TmdbDatasource(),
-);
+final movieDatasourceProvider = Provider.autoDispose<MovieDatasource>((ref) {
+  final httpClient = ref.watch(httpClientProvider);
+  return TmdbDatasource(httpClient: httpClient);
+});

47f8e55770ab650e0377551be598bfa532394daf
Santino Albornoz <sanalbor32@gmail.com>
2026-03-17 20:45:46 -0300

    feat: setup and initialize Hive local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:cine_scope/features/movies/data/models/local/movie_local_model.dart';
+import 'package:cine_scope/features/movies/data/models/local/movie_summary_local_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "lib/.env");
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(MovieLocalModelAdapter());
+  Hive.registerAdapter(MovieSummaryLocalModelAdapter());
+
+  await Hive.openBox<MovieLocalModel>('favorites');
+  await Hive.openBox<MovieLocalModel>('watchLater');
+
   runApp(ProviderScope(child: MainApp()));
 }
 

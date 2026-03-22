ebd5f05302db15adabeb1d2fd5a86169da3321ac
Santino Albornoz <sanalbor32@gmail.com>
2026-03-21 21:10:28 -0300

    feat(movies): handle async states in popular movies list widget

--- lib/features/movies/presentation/popular_movies_list.dart
+++ lib/features/movies/presentation/popular_movies_list.dart
@@ -1,33 +1,37 @@
 import 'package:cine_scope/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PopularMoviesList extends StatelessWidget {
+class PopularMoviesList extends ConsumerWidget {
   const PopularMoviesList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final movies = [
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-      Placeholder(),
-    ];
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(_mockListProvider);
 
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 0.6,
-        mainAxisSpacing: AppSpacing.lg,
-        crossAxisSpacing: AppSpacing.lg,
-      ),
-      itemCount: movies.length,
-      itemBuilder: (context, index) {
-        return movies[index];
+    return movies.when(
+      data: (movies) {
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+            mainAxisSpacing: AppSpacing.lg,
+            crossAxisSpacing: AppSpacing.lg,
+          ),
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return movies[index];
+          },
+        );
       },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stack) => Center(child: Text(error.toString())),
     );
   }
 }
+
+final _mockListProvider = FutureProvider<List<Widget>>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+
+  return List.generate(10, (index) => Placeholder());
+});

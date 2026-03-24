a6f8ab4877340a0bed3ccfe357b941d7a2551723
Santino Albornoz <sanalbor32@gmail.com>
2026-03-24 12:00:52 -0300

    feat(movies): add mock MovieSummary list to mock list provider in PopularMoviesList

--- lib/features/movies/presentation/popular_movies_list.dart
+++ lib/features/movies/presentation/popular_movies_list.dart
@@ -1,4 +1,6 @@
 import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
+import 'package:cine_scope/features/movies/presentation/movie_card.dart';
 import 'package:cine_scope/features/movies/presentation/skeletons/movie_list_skeleton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -34,5 +36,38 @@ class PopularMoviesList extends ConsumerWidget {
 final _mockListProvider = FutureProvider<List<Widget>>((ref) async {
   await Future.delayed(const Duration(seconds: 3));
 
-  return List.generate(10, (index) => Placeholder());
+  final mockMovies = [
+    MovieSummary(
+      id: 550,
+      title: 'Fight Club',
+      posterPath:
+          'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+      voteAverage: 8.433,
+      releaseDate: '1999-10-15',
+      adult: false,
+      genreIds: [18, 53, 35],
+    ),
+    MovieSummary(
+      id: 680,
+      title: 'Pulp Fiction',
+      posterPath:
+          'https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg',
+      voteAverage: 8.49,
+      releaseDate: '1994-09-10',
+      adult: false,
+      genreIds: [53, 80],
+    ),
+    MovieSummary(
+      id: 13,
+      title: 'Forrest Gump',
+      posterPath:
+          'https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg',
+      voteAverage: 8.47,
+      releaseDate: '1994-06-23',
+      adult: false,
+      genreIds: [35, 18, 10749],
+    ),
+  ];
+
+  return mockMovies.map((movie) => MovieCard(movie: movie)).toList();
 });

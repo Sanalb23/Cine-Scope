7b0f2b4205fc4f2e564ea8756b7d56c457d67f2a
Santino Albornoz <sanalbor32@gmail.com>
2026-03-26 21:20:27 -0300

    test(movies): add test for movie repository for favorites and watch list

---
+++ test/features/movies/data/repositories/movie_repository_test.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:cine_scope/features/movies/data/datasource/movie_local_datasource.dart';
+import 'package:cine_scope/features/movies/data/datasource/movie_remote_datasource.dart';
+import 'package:cine_scope/features/movies/data/models/movie_model.dart';
+import 'package:cine_scope/features/movies/data/repositories/movie_repository_impl.dart';
+import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../datasource/movie_remote_datasource_test.dart';
+
+class MockMovieRemoteDatasource extends Mock implements MovieRemoteDatasource {}
+
+class MockMovieLocalDatasource extends Mock implements MovieLocalDatasource {}
+
+void main() {
+  late MockMovieRemoteDatasource mockMovieRemoteDatasource;
+  late MockMovieLocalDatasource mockMovieLocalDatasource;
+  late MovieRepositoryImpl movieRepository;
+
+  setUp(() {
+    mockMovieRemoteDatasource = MockMovieRemoteDatasource();
+    mockMovieLocalDatasource = MockMovieLocalDatasource();
+    movieRepository = MovieRepositoryImpl(
+      remoteDatasource: mockMovieRemoteDatasource,
+      localDatasource: mockMovieLocalDatasource,
+    );
+  });
+
+  group('favorite movies test', () {
+    test('should return a list of favorite movies', () async {
+      //local datasource arrange
+      final fakeIds = [1, 2];
+
+      when(
+        () => mockMovieLocalDatasource.getFavorites(),
+      ).thenAnswer((_) => fakeIds);
+
+      //remote datasource arrange
+      final fakeMovie = MovieModel.fromJson(
+        jsonDecode(fixture('movie_details')),
+      );
+
+      when(
+        () => mockMovieRemoteDatasource.getMovieById(id: 1),
+      ).thenAnswer((_) async => fakeMovie);
+
+      when(
+        () => mockMovieRemoteDatasource.getMovieById(id: 2),
+      ).thenAnswer((_) async => fakeMovie);
+
+      //act
+      final result = await movieRepository.getFavorites();
+
+      //assert
+      expect(result, isA<List<MovieSummary>>());
+      expect(result.length, 2);
+    });
+  });
+
+  test('should return a list of movies in watchlist', () async {
+    //local datasource arrange
+    final fakeIds = [1, 2];
+
+    when(
+      () => mockMovieLocalDatasource.getWatchList(),
+    ).thenAnswer((_) => fakeIds);
+
+    //remote datasource arrange
+    final fakeMovie = MovieModel.fromJson(jsonDecode(fixture('movie_details')));
+
+    when(
+      () => mockMovieRemoteDatasource.getMovieById(id: 1),
+    ).thenAnswer((_) async => fakeMovie);
+
+    when(
+      () => mockMovieRemoteDatasource.getMovieById(id: 2),
+    ).thenAnswer((_) async => fakeMovie);
+
+    //act
+    final result = await movieRepository.getWatchList();
+
+    //assert
+    expect(result, isA<List<MovieSummary>>());
+    expect(result.length, 2);
+  });
+}

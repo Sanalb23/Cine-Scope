292fafd654dc852d2ace06663fd971212a90d7bb
Santino Albornoz <sanalbor32@gmail.com>
2026-03-28 11:25:34 -0300

    feat(movies): add auto-dispose movie provider with cache expiration timer

--- lib/features/movies/domain/providers/notifiers/remote/movie_provider.dart
+++ lib/features/movies/domain/providers/notifiers/remote/movie_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cine_scope/features/movies/domain/entities/movie.dart';
 import 'package:cine_scope/features/movies/domain/providers/movie_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,5 +8,13 @@ final movieProvider = FutureProvider.autoDispose.family<Movie, int>((
   ref,
   id,
 ) async {
+  final link = ref.keepAlive();
+
+  final timer = Timer(const Duration(minutes: 3), () {
+    link.close();
+  });
+
+  ref.onDispose(() => timer.cancel());
+
   return ref.read(movieRepositoryProvider).getMovieById(id: id);
 });

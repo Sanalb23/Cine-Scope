9b328d0a1f4ecba4c2aee826f6847af1feb061b0
Santino Albornoz <sanalbor32@gmail.com>
2026-03-29 00:34:30 -0300

    feat(movies): add auto-dispose and cache expiration logic to similarMoviesProvider

--- lib/features/movies/domain/providers/notifiers/remote/similar_movies_provider.dart
+++ lib/features/movies/domain/providers/notifiers/remote/similar_movies_provider.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
 import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
 import 'package:cine_scope/features/movies/domain/providers/movie_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final similarMoviesProvider = FutureProvider.autoDispose
     .family<List<MovieSummary>, int>((ref, id) async {
+      final link = ref.keepAlive();
+
+      final timer = Timer(const Duration(minutes: 3), () {
+        link.close();
+      });
+
+      ref.onDispose(() => timer.cancel());
+
       return ref.read(movieRepositoryProvider).getSimilarMovies(id: id);
     });

7c0a0c41f8b256b3409c0c7e00982f61e2659354
Santino Albornoz <sanalbor32@gmail.com>
2026-04-03 16:26:20 -0300

    feat(movies): implement favorite logic in MovieDetailsScreen watch list button - Convert WatchListButton to a ConsumerWidget to manage its state

--- lib/features/movies/presentation/movie_details_screen/watch_list_button.dart
+++ lib/features/movies/presentation/movie_details_screen/watch_list_button.dart
@@ -1,13 +1,22 @@
+import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/watch_list_provider.dart';
 import 'package:cine_scope/features/movies/presentation/movie_details_screen/appbar_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class WatchListButton extends StatelessWidget {
+class WatchListButton extends ConsumerWidget {
   const WatchListButton({super.key, required this.movieId});
 
   final int movieId;
 
   @override
-  Widget build(BuildContext context) {
-    return AppBarButton(icon: Icons.watch_later_outlined, onPressed: () {});
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isInWatchList = ref.watch(isInWatchListProvider(movieId));
+
+    return AppBarButton(
+      icon: isInWatchList ? Icons.watch_later : Icons.watch_later_outlined,
+      onPressed: () {
+        ref.read(watchListProvider.notifier).toggleInWatchList(movieId);
+      },
+    );
   }
 }

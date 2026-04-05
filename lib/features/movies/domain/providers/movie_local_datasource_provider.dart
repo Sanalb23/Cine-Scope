ad0a8b957becbe3bf38a12ce3b29d93a8a8ca1e2
Santino Albornoz <sanalbor32@gmail.com>
2026-04-05 00:08:29 -0300

    refactor: relocate prefs instance provider to core

--- lib/features/movies/domain/providers/movie_local_datasource_provider.dart
+++ lib/features/movies/domain/providers/movie_local_datasource_provider.dart
@@ -1,6 +1,6 @@
 import 'package:cine_scope/features/movies/data/datasource/movie_local_datasource.dart';
 import 'package:cine_scope/features/movies/data/datasource/movie_local_datasource_impl.dart';
-import 'package:cine_scope/features/movies/domain/providers/prefs_instance_provider.dart';
+import 'package:cine_scope/core/providers/prefs_instance_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final movieLocalDatasourceProvider = Provider.autoDispose<MovieLocalDatasource>(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:cine_scope/core/theme/app_theme.dart';
 import 'package:cine_scope/features/home/screens/home_screen.dart';
-import 'package:cine_scope/features/movies/domain/providers/prefs_instance_provider.dart';
+import 'package:cine_scope/core/providers/prefs_instance_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';

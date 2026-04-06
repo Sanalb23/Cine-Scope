642bd16687356dd0909accc51582d3ddb96d9f05
Santino Albornoz <sanalbor32@gmail.com>
2026-04-06 12:34:30 -0300

    refactor: extract and relocate error TryAgainWidget widget to core

---
+++ lib/core/widgets/try_again_later.dart
@@ -0,0 +1,24 @@
+import 'package:cine_scope/core/extensions/context_extensions.dart';
+import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class TryAgainLater extends StatelessWidget {
+  const TryAgainLater({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: .center,
+      spacing: AppSpacing.md,
+      children: [
+        Icon(Icons.error_outline, size: 48),
+        Text('Error', style: context.textTheme.headlineSmall),
+        Text('Please try again later', style: context.textTheme.bodyMedium),
+        const SizedBox(height: AppSpacing.md),
+        ElevatedButton(onPressed: onPressed, child: Text('Retry')),
+      ],
+    );
+  }
+}
--- lib/features/movies/presentation/movie_details_screen/movie_details_screen.dart
+++ lib/features/movies/presentation/movie_details_screen/movie_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cine_scope/core/extensions/context_extensions.dart';
 import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:cine_scope/core/widgets/try_again_later.dart';
 import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/movie_provider.dart';
 import 'package:cine_scope/features/movies/presentation/movie_details_screen/appbar_button.dart';
 import 'package:cine_scope/features/movies/presentation/movie_details_screen/favorite_button.dart';
@@ -144,22 +145,8 @@ class MovieDetailsScreen extends ConsumerWidget {
         },
         error: (error, stackTrace) {
           return Center(
-            child: Column(
-              mainAxisAlignment: .center,
-              spacing: AppSpacing.md,
-              children: [
-                Icon(Icons.error_outline, size: 48),
-                Text('Error', style: context.textTheme.headlineSmall),
-                Text(
-                  'Please try again later',
-                  style: context.textTheme.bodyMedium,
-                ),
-                const SizedBox(height: AppSpacing.md),
-                ElevatedButton(
-                  onPressed: () => ref.invalidate(movieProvider(id)),
-                  child: Text('Retry'),
-                ),
-              ],
+            child: TryAgainLater(
+              onPressed: () => ref.invalidate(movieProvider(id)),
             ),
           );
         },

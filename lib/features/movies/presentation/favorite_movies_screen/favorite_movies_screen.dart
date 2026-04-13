b33dc78bd18d656eed858cb567d522dc778453b1
Santino Albornoz <sanalbor32@gmail.com>
2026-04-13 12:30:20 -0300

    feat(presentation): integrate paginated favorites state into UI
    - Convert FavoriteMoviesScreen to ConsumerWidget.
    - Handle loading, error, and data states using AsyncValue.
    - Implement pull-to-refresh and infinite scroll callbacks in MoviesList.

--- lib/features/movies/presentation/favorite_movies_screen/favorite_movies_screen.dart
+++ lib/features/movies/presentation/favorite_movies_screen/favorite_movies_screen.dart
@@ -1,15 +1,30 @@
 import 'package:cine_scope/core/extensions/context_extensions.dart';
+import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/favorite_movies_provider.dart';
+import 'package:cine_scope/features/movies/presentation/utils/movies_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoriteMoviesScreen extends StatelessWidget {
+class FavoriteMoviesScreen extends ConsumerWidget {
   const FavoriteMoviesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(favoriteMoviesProvider);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Favorites', style: context.textTheme.headlineSmall),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppSpacing.lg),
+        child: MoviesList(
+          movies: movies,
+          onFetchMore: () =>
+              ref.read(favoriteMoviesProvider.notifier).fetchMore(),
+          onRefresh: () async => ref.refresh(favoriteMoviesProvider),
+        ),
       ),
     );
   }

cb0b26035ef92bd1fccf6c4f6913a339d6996651
Santino Albornoz <sanalbor32@gmail.com>
2026-04-14 18:46:14 -0300

    feat(presentation): integrate paginated watch list state into UI
        - Convert WatchListScreen to ConsumerWidget
        - Implement pull-to-refresh and infinite scroll callbacks in MoviesList.

--- lib/features/movies/presentation/watch_list_screen/watch_list_screen.dart
+++ lib/features/movies/presentation/watch_list_screen/watch_list_screen.dart
@@ -1,15 +1,30 @@
 import 'package:cine_scope/core/extensions/context_extensions.dart';
+import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/watch_list_movies_provider.dart';
+import 'package:cine_scope/features/movies/presentation/utils/movies_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class WatchListScreen extends StatelessWidget {
+class WatchListScreen extends ConsumerWidget {
   const WatchListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(watchListMoviesProvider);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Watch List', style: context.textTheme.headlineSmall),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppSpacing.lg),
+        child: MoviesList(
+          movies: movies,
+          onFetchMore: () =>
+              ref.read(watchListMoviesProvider.notifier).fetchMore(),
+          onRefresh: () async => ref.refresh(watchListMoviesProvider),
+        ),
       ),
     );
   }

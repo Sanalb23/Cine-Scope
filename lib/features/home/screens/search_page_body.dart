6e99d109e906d9161c612414e1cf7646dc2847a3
Santino Albornoz <sanalbor32@gmail.com>
2026-04-15 13:26:08 -0300

    feat(home): add basic controller to search bar

--- lib/features/home/screens/search_page_body.dart
+++ lib/features/home/screens/search_page_body.dart
@@ -1,8 +1,21 @@
 import 'package:cine_scope/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class SearchPageBody extends StatelessWidget {
+class SearchPageBody extends StatefulWidget {
   const SearchPageBody({super.key});
+
+  @override
+  State<SearchPageBody> createState() => _SearchPageBodyState();
+}
+
+class _SearchPageBodyState extends State<SearchPageBody> {
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +25,12 @@ class SearchPageBody extends StatelessWidget {
           padding: WidgetStatePropertyAll(
             EdgeInsets.symmetric(horizontal: AppSpacing.lg),
           ),
+          controller: _searchController,
           trailing: [Icon(Icons.search)],
           hintText: 'Search...',
+          onChanged: (value) {
+            // TODO: implement search
+          },
         ),
       ],
     );

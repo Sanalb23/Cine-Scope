97974e2bfe51f807db63c856155583089a91e3a9
Santino Albornoz <sanalbor32@gmail.com>
2026-04-15 20:40:59 -0300

    feat(search): implement search movies list and query integration
    - Convert SearchPageBody to ConsumerStatefulWidget to use Riverpod.
    
    - Update searchQueryProvider on SearchBar input changes.
    
    - Add _MoviesList widget to observe search results and handle pagination with fetchMore.

--- lib/features/home/screens/search_page_body.dart
+++ lib/features/home/screens/search_page_body.dart
@@ -1,14 +1,18 @@
 import 'package:cine_scope/core/theme/app_theme.dart';
+import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/search_movies/search_movies_provider.dart';
+import 'package:cine_scope/features/movies/domain/providers/notifiers/remote/search_movies/search_query_provider.dart';
+import 'package:cine_scope/features/movies/presentation/utils/movies_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SearchPageBody extends StatefulWidget {
+class SearchPageBody extends ConsumerStatefulWidget {
   const SearchPageBody({super.key});
 
   @override
-  State<SearchPageBody> createState() => _SearchPageBodyState();
+  ConsumerState<SearchPageBody> createState() => _SearchPageBodyState();
 }
 
-class _SearchPageBodyState extends State<SearchPageBody> {
+class _SearchPageBodyState extends ConsumerState<SearchPageBody> {
   final _searchController = TextEditingController();
 
   @override
@@ -20,19 +24,38 @@ class _SearchPageBodyState extends State<SearchPageBody> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: AppSpacing.lg,
       children: [
         SearchBar(
-          padding: WidgetStatePropertyAll(
+          padding: const WidgetStatePropertyAll(
             EdgeInsets.symmetric(horizontal: AppSpacing.lg),
           ),
           controller: _searchController,
-          trailing: [Icon(Icons.search)],
+          trailing: [const Icon(Icons.search)],
           hintText: 'Search...',
           onChanged: (value) {
-            // TODO: implement search
+            ref.read(searchQueryProvider.notifier).setSearchQuery(value);
           },
         ),
+
+        const Expanded(child: _MoviesList()),
       ],
+    );
+  }
+}
+
+class _MoviesList extends ConsumerWidget {
+  const _MoviesList();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final query = ref.watch(searchQueryProvider);
+
+    final results = ref.watch(searchMoviesProvider(query));
+
+    return MoviesList(
+      movies: results,
+      onFetchMore: ref.read(searchMoviesProvider(query).notifier).fetchMore,
     );
   }
 }

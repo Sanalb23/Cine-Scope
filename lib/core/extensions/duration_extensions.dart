96ca0295ce0f33394a5a5873281d38e722383fb9
Santino Albornoz <sanalbor32@gmail.com>
2026-04-16 00:19:47 -0300

    feat(core): improve duration string formatting for exact hours or minutes

--- lib/core/extensions/duration_extensions.dart
+++ lib/core/extensions/duration_extensions.dart
@@ -2,6 +2,15 @@ extension DurationExtensions on Duration {
   String get hoursAndMinutesString {
     final hours = inMinutes ~/ 60;
     final minutes = inMinutes % 60;
+
+    if (hours == 0) {
+      return '$minutes min';
+    }
+
+    if (minutes == 0) {
+      return '$hours h';
+    }
+
     return '$hours h $minutes min';
   }
 }

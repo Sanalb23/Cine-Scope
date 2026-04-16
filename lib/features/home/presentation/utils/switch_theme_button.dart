9f8fc91a3a575f4571b6990d6ea501867ef60d91
Santino Albornoz <sanalbor32@gmail.com>
2026-04-16 18:41:34 -0300

    feat(home): create basic switch theme button

---
+++ lib/features/home/presentation/utils/switch_theme_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SwitchThemeButton extends StatelessWidget {
+  const SwitchThemeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: () {}, icon: Icon(Icons.light_mode));
+  }
+}

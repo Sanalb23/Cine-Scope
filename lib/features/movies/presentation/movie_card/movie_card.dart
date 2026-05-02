cb089a8c4b79c142f7a31ece4c4976997cdddb1e
Santino Albornoz <sanalbor32@gmail.com>
2026-05-02 18:29:45 -0300

    feat(l18n): localize popup menu actions in movie card
    Replace hardcoded 'Remove from...' and 'Add to...' strings in the
    MovieCard popup menu with localized keys using easy_localization.
    
    Add 'remove_from' and 'add_to' translation keys with positional
    arguments to both en-US and es-MX translation files.

--- lib/features/movies/presentation/movie_card/movie_card.dart
+++ lib/features/movies/presentation/movie_card/movie_card.dart
@@ -7,6 +7,7 @@ import 'package:cine_scope/features/movies/presentation/movie_card/genre_tags_ro
 import 'package:cine_scope/features/movies/presentation/movie_details_screen/movie_details_screen.dart';
 import 'package:cine_scope/features/movies/presentation/utils/confirm_removal_dialog.dart';
 import 'package:cine_scope/features/movies/presentation/utils/movie_poster.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -26,7 +27,11 @@ class MovieCard extends ConsumerWidget {
           spacing: AppSpacing.md,
           children: [
             Icon(isInFavorites ? Icons.bookmark : Icons.bookmark_add_outlined),
-            Text(isInFavorites ? 'Remove from favorites' : 'Add to favorites'),
+            Text(
+              isInFavorites
+                  ? 'remove_from'.tr(args: ['favorites'.tr()])
+                  : 'add_to'.tr(args: ['favorites'.tr()]),
+            ),
           ],
         ),
       ),
@@ -39,7 +44,9 @@ class MovieCard extends ConsumerWidget {
               isInWatchList ? Icons.watch_later : Icons.watch_later_outlined,
             ),
             Text(
-              isInWatchList ? 'Remove from watch list' : 'Add to watch list',
+              isInWatchList
+                  ? 'remove_from'.tr(args: ['watch_list'.tr()])
+                  : 'add_to'.tr(args: ['watch_list'.tr()]),
             ),
           ],
         ),

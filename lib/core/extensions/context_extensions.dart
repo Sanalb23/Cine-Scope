3478ec8c8c2abfc84ec6224b26ef753e05482d0c
Santino Albornoz <sanalbor32@gmail.com>
2026-05-26 22:57:07 -0300

    feat(core): add screen diagonal calculation to context extensions

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 extension ContextExtensions on BuildContext {
@@ -6,4 +8,5 @@ extension ContextExtensions on BuildContext {
   ColorScheme get colors => theme.colorScheme;
   double get screenWidth => MediaQuery.of(this).size.width;
   double get screenHeight => MediaQuery.of(this).size.height;
+  double get screenDiagonal => sqrt(pow(screenWidth, 2) + pow(screenHeight, 2));
 }

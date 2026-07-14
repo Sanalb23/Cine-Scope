6c7d6f5847a414100df21ae7eed6002770ad4c42
Santino Albornoz <sanalbor32@gmail.com>
2026-07-13 21:18:19 -0300

    refactor(notifications): relocate NotificationService to core services

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:cine_scope/core/theme/app_theme_provider.dart';
 import 'package:cine_scope/core/theme/data/app_theme.dart';
 import 'package:cine_scope/features/home/presentation/home_screen.dart';
 import 'package:cine_scope/core/providers/prefs_instance_provider.dart';
-import 'package:cine_scope/core/features/notifications/notification_service.dart';
+import 'package:cine_scope/core/services/notification_service.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';

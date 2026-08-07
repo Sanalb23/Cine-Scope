a79e8a0e81338192ed4b2575c00c39e77c495081
Santino Albornoz <sanalbor32@gmail.com>
2026-08-06 21:05:16 -0300

    feat(movies-presentation): add empty state handling to MoviesList widget

--- lib/features/movies/presentation/utils/movies_list.dart
+++ lib/features/movies/presentation/utils/movies_list.dart
@@ -2,6 +2,7 @@ import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
 import 'package:cine_scope/features/movies/presentation/movie_card/movie_card.dart';
 import 'package:cine_scope/features/movies/presentation/utils/movie_card_skeleton.dart';
 import 'package:cine_scope/features/movies/presentation/utils/movies_grid.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class MoviesList extends StatelessWidget {
@@ -12,6 +13,10 @@ class MoviesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (movies.isEmpty && !isLoading) {
+      return Center(child: Text('no_movies_found'.tr()));
+    }
+
     return MoviesGrid(
       children: [
         ...movies.map((movie) => MovieCard(movie: movie)),

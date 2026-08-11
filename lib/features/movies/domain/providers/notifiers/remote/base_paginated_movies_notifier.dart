def5953192d26f02254be09c0ee5e806edd23b8a
Santino Albornoz <sanalbor32@gmail.com>
2026-08-11 17:35:43 -0300

    feat(movies): add localeProvider watch to movies notifier to fetch new data on language change

--- lib/features/movies/domain/providers/notifiers/remote/base_paginated_movies_notifier.dart
+++ lib/features/movies/domain/providers/notifiers/remote/base_paginated_movies_notifier.dart
@@ -1,9 +1,17 @@
+import 'package:cine_scope/core/providers/locale_provider.dart';
 import 'package:cine_scope/features/movies/data/utils/preload_posters.dart';
 import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
 import 'package:cine_scope/features/pagination/paginated_notifier.dart';
+import 'package:cine_scope/features/pagination/models/paginated_state.dart';
 
 abstract class BasePaginatedMoviesNotifier
     extends PaginatedNotifier<MovieSummary> {
+  @override
+  PaginatedState<MovieSummary> build() {
+    ref.watch(localeProvider);
+    return super.build();
+  }
+
   @override
   Future<void> preloadFn(List<MovieSummary> items) async {
     await preloadPosters(items);

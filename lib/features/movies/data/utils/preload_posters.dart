5ac2268cd78968cb8131003abdd4ca1a41c7c0a8
Santino Albornoz <sanalbor32@gmail.com>
2026-08-20 23:38:50 -0300

    fix(movies): skip poster preloading during tests and on web

--- lib/features/movies/data/utils/preload_posters.dart
+++ lib/features/movies/data/utils/preload_posters.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:cine_scope/features/movies/domain/entities/movie_summary.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 Future<void> preloadPosters(List<MovieSummary> movies) async {
+  if (!kIsWeb && Platform.environment.containsKey('FLUTTER_TEST')) return;
+
   await Future.wait(
     movies.map((movie) async {
       if (movie.posterPath == null) return;
